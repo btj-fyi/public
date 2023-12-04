65f1bebade80972bc7dce0c582d4a0be532e2c30
btj-fyi <109888835+btj-fyi@users.noreply.github.com>
2023-12-03 21:05:53 -0800

    Chinese 1 - CS Integration
    Author:       Benjamin Johnson
    College:      Mission College
    Class:        Chinese 1 (73515)
    Date:         Dec 12, 2023
    Description:  Integrate CS with Languages & Culture

---
+++ cs_integration.py
@@ -0,0 +1,9 @@
+# Author:       Benjamin Johnson
+# College:      Mission College
+# Class:        Chinese 1 (73515)
+# Date:         Dec 12, 2023
+# Description:  Integrate CS with Languages & Culture
+
+from sketchpy import canvas
+obj = canvas.sketch_from_image(r'chinese_architecture.jpg')
+obj.draw(threshold = 45)
