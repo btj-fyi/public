52e32bf8374b4b131180b011fbdddaadff2500cb
Benjamin <me@btj.fyi>
2024-01-27 12:17:23 -0800

    Corrected issues with index addresses

--- Life at Meta - Sample Interview Questions & Solutions/2D Spiral Array.py
+++ Life at Meta - Sample Interview Questions & Solutions/2D Spiral Array.py
@@ -32,81 +32,77 @@ input = 8
 # segment 1,2, and 3, must be stopped
 # segments after 3, may continue until the next coordinate is already occupied. Then turn
 def spiral(n):
-    m = n
-    numbers = list(range(1, (m * n) + 1))
+    matrix = [[0 for _ in range(n)] for _ in range(n)]
+    print(matrix)
+    numbers = list(range(1, (n * n) + 1, 1))
     print(numbers)
-    matrix = [[0 for _ in range(m)] for _ in range(n)]
     i = 0
     j = 0
-    direction = "up"
-    segment = 1
-    i = 0
-    dimension = n - 1
-    while True:
-        if (segment == 1) and (i <= dimension):
-            matrix[0][i] = numbers.pop(0)
-            i += 1
-        elif segment == 1:
-            segment = 2
-            i = 1
-        if (segment == 2) and (i <= dimension):
-            matrix[i][n - 1] = numbers.pop(0)
-            i += 1
-        elif segment == 2:
-            segment = 3
-            i = 1
-        if (segment == 3) and (i <= dimension):
-            matrix[m - 1][(n - 1) - i] = numbers.pop(0)
-            i += 1
-        elif segment == 3:
-            segment = 4
-            i = 0
-        if segment == 4:
+    row = 0
+    col = 0
+    c = 0
+    if c == 0:
+        # go right, length
+        for x in range(0, n, 1):
+            j = x
+            matrix[i][j] = numbers.pop(0)
+        c += 1
+        row, col = i, j
+        print(i, j)
+    if c == 1:
+        # go down, length-1
+        for x in range(row + 1, n, 1):
+            i = x
+            matrix[i][j] = numbers.pop(0)
+        c += 1
+        row, col = i, j
+        print(i, j)
+    if c == 2:
+        # go left, length-1
+        for x in range(col - 1, -1, -1):
+            j = x
+            matrix[i][j] = numbers.pop(0)
+        c += 1
+        row, col = i, j
+        print(i, j)
+        direction = "up"
+    if c > 2:
+        while True:
             match direction:
-                # add 1 to i, up until none zero value
                 case "up":
-                    i += 1
-                    if matrix[dimension - i][0] == 0:
-                        matrix[dimension - i][0] = numbers.pop(0)
-                        i += 1
-                    if matrix[dimension - i][0] != 0:
-                        i -= 1
-                        matrix[dimension - i][0] = numbers.pop(0)
-                        i = 0
-                        direction = "right"
-                # add 1 to j, up until none zero value
-                case "right":
-                    j += 1
-                    if matrix[i][dimension - j] == 0:
-                        matrix[i][dimension - j] = numbers.pop(0)
-                        j += 1
-                    if matrix[i][dimension - j] != 0:
-                        j -= 1
-                        matrix[i][dimension - j] = numbers.pop(0)
-                        direction = "down"
-
-                # subtract 1 from i, up until none zero value
-                case "down":
+                    # go up, till none zero
                     i -= 1
-                    if matrix[dimension + i][j]:
-                        matrix[dimension + i][j] = numbers.pop(0)
-                    i -= 1
-                    if matrix[i][j] != 0:
-                        i += 1
-                        matrix[dimension + i][j] = numbers.pop(0)
-                        direction = "left"
-                # subtract 1 from j, up until none zero value
-                case "left":
-                    j -= 1
-                    if matrix[i][j] != 0:
-                        j += 1
+                    if matrix[i][j] == 0:
                         matrix[i][j] = numbers.pop(0)
-        if not numbers:
-            break
-    for row in matrix:
-        for val in row:
-            print(val, end=" ")
-        print()
+                    else:
+                        i += 1
+                        direction = "right"
+                case "right":
+                    # go right
+                    j += 1
+                    if matrix[i][j] == 0:
+                        matrix[i][j] = numbers.pop(0)
+                    else:
+                        j -= 1
+                        direction = "down"
+                case "down":
+                    # go down
+                    i += 1
+                    if matrix[i][j] == 0:
+                        matrix[i][j] = numbers.pop(0)
+                    else:
+                        i -= 1
+                        direction = "left"
+                case "left":
+                    # go left
+                    j -= 1
+                    if matrix[i][j] == 0:
+                        matrix[i][j] = numbers.pop(0)
+                    else:
+                        j += 1
+                        direction = "up"
+            if not numbers:
+                break
 
 
-spiral(4)
+spiral(8)
